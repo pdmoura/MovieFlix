919529fecd03b54cad3ccc20fa449d7f67a1874d
pdmoura <sudwarriormoura@gmail.com>
2025-11-26 08:30:52 -0300

    feat: implement fetchTrendingMovies api call

--- js/Api.mjs
+++ js/Api.mjs
@@ -1,9 +1,8 @@
-import { API_KEY, BASE_URL } from './Config.mjs';
+import { API_KEY, BASE_URL } from "./Config.mjs";
 
 export async function fetchTrendingMovies() {
-    const url = `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log("API Connection Successful:", data);
-    return data.results;
+	const url = `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`;
+	const response = await fetch(url);
+	const data = await response.json();
+	return data.results.slice(0, 10);
 }
