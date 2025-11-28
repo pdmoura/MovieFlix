d3045e7d78c2b311c5b54ce16d20cae4ad24bf3c
pdmoura <sudwarriormoura@gmail.com>
2025-11-28 07:41:36 -0300

    fix: changed styles to enhance img quality and website perfomance

--- js/Config.mjs
+++ js/Config.mjs
@@ -1,4 +1,4 @@
 export const API_KEY = "3fd2be6f0c70a2a598f084ddfb75487c"; // Use your actual key, this I generated with AI xD
 export const BASE_URL = 'https://api.themoviedb.org/3';
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w300';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w342';
 export const FALLBACK_IMAGE = "https://placehold.co/320x450/black/white?text=No+Image+\nFound";
